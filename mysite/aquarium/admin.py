288c282d0489e787f356c5ab057888f631957d96
VytautasZ12 <vzvytautasz@gmail.com>
2023-09-15 14:36:15 +0300

    Created Fish spieces and Fish models, created admin page.

--- mysite/aquarium/admin.py
+++ mysite/aquarium/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Fish, FishSpieces
 
 # Register your models here.
+admin.site.register(Fish)
+admin.site.register(FishSpieces)
--- mysite/aquarium/models.py
+++ mysite/aquarium/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
+
 # Create your models here.
+
+class FishSpieces(models.Model):
+    rusies_pavadinimas = models.CharField(verbose_name="Zuvu rusies pavadinimas", max_length=100)
+    aprasymas = models.TextField(verbose_name="Rusies aprasymas", max_length=1000)
+
+
+    def __str__(self):
+        return self.rusies_pavadinimas
+
+
+class Fish(models.Model):
+    pavadinimas = models.CharField(verbose_name="Pavadinimas", max_length=100)
+    kilme = models.CharField(verbose_name="Kilme", max_length=50)
+    aprasymas = models.TextField(verbose_name="Aprasymas", max_length=1000, help_text="Trumpas zuvies aprasymas")
+    rusis = models.ForeignKey("FishSpieces", on_delete=models.SET_NULL, null=True)
+
+
+    def __str__(self):
+        return self.pavadinimas
