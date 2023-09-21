7364365406a997e36c6e0f74e1a71c77674c0eb2
VytautasZ12 <vzvytautasz@gmail.com>
2023-09-21 16:01:34 +0300

    Made search bar in admin site, adjusted base.htm and index.html.

--- mysite/aquarium/admin.py
+++ mysite/aquarium/admin.py
@@ -3,10 +3,10 @@ from .models import Fish, FishSpieces
 
 
 class FishAdmin(admin.ModelAdmin):
-    list_display = ('pavadinimas', 'kilme', 'rusis')
-    list_filter = ('kilme', 'rusis', 'pavadinimas')
+    list_display = ['pavadinimas', 'kilme', 'rusis']
+    list_filter = ['kilme', 'rusis', 'pavadinimas']
 
-    search_fields = ["id", "fish__kilme", "fish__rusis"]
+    search_fields = ['pavadinimas']
 
 
 # Register your models here.
